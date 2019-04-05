a1f72ff90571c6aa54e5a475d2f4cb73ed14215f
Charlie <phunghiep2709@gmail.com>
2019-04-05 17:20:04 +0300

    add use Doc

--- src/Hooks/useDoc.ts
+++ src/Hooks/useDoc.ts
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
 import { db } from "../firebase/firebase";
+import { DocumentSnapshot } from "@firebase/firestore-types";
 
 const useCollection = <T>(path: string): T => {
   const [doc, setDoc] = useState<any | null>();
 
   useEffect(() => {
-    return db.doc(path).onSnapshot(ss => {
+    return db.doc(path).onSnapshot((ss: DocumentSnapshot) => {
       setDoc({
         ...ss.data(),
         id: ss.id
